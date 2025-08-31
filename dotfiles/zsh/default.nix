ab370739956a478f6e43d030b0e45548e5ad1c4f
JK13xyz <jens@jk13.dev>
2025-08-31 15:03:08 +0200

    Fixed p10k

--- dotfiles/zsh/default.nix
+++ dotfiles/zsh/default.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ...  }:
 
 {
+  home.file.".p10k.zsh".source = ./.p10k.zsh;
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -20,11 +22,11 @@
         name = "powerlevel10k";
         src = pkgs.zsh-powerlevel10k;
       }
-      {
-        name = "powerlevel10k-config";
-        src = ./.p10k.zsh;
-        file = ".p10k.zsh";
-      }
     ];
+
+    initContent = ''
+      source ~/.zsh/plugins/powerlevel10k/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+    '';
   };
 }
